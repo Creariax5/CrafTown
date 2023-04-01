16bc53794ca52920f9a6a731c92f539108ce14dc
Creariax5 <63298524+Creariax5@users.noreply.github.com>
2023-04-01 11:06:29 +0200

    setup craftTown

---
+++ src/main/java/elc/florian/Main.java
@@ -0,0 +1,10 @@
+package elc.florian;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("CraftTown started !");
+    }
+}
